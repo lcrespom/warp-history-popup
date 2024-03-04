422978b4bac51ae7e4cece01a23e449f6a6bb75a
Luis Crespo <lcrespom@gmail.com>
2024-03-04 20:02:49 +0100

    Trying to type string...

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
+import robot from 'robotjs'
 import { registerHotkey } from './hotkey'
 import { showHistoryMenu } from './history'
 
-function registerHotKeys() {
+function registerHotKeys(command: string) {
     registerHotkey({
         key: 'PAGE UP',
         modifiers: ['LEFT CTRL'],
-        callback: () => console.log('ctrl page up')
+        callback: () => robot.typeString('ToDo history')
     })
     registerHotkey({
         key: 'PAGE DOWN',
         modifiers: ['LEFT CTRL'],
-        callback: () => console.log('ctrl page down')
+        callback: () => robot.typeString('ToDo dirHistory')
     })
 }
 
 async function main() {
-    if (process.argv[process.argv.length - 1] == 'history') {
-        showHistoryMenu()
-    } else {
-        registerHotKeys()
+    switch (process.argv[process.argv.length - 1]) {
+        case 'history':
+            showHistoryMenu()
+            break
+        case 'dirHistory':
+            console.log('ToDo: dir history')
+            break
+        default:
+            registerHotKeys(process.argv.join(' '))
     }
 }
 
