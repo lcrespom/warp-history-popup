57a5d6203341a211d68579cf29c10f0835166a89
Luis Crespo <lcrespom@gmail.com>
2024-03-04 11:54:19 +0100

    Initial version

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import keypress from 'keypress'
+import {
+    hideCursor,
+    showCursor,
+    tableMenu,
+    computeTableLayout
+} from 'node-terminal-menu'
+
+function listenKeyboard(kbHandler) {
+    process.stdin.setRawMode(true)
+    process.stdin.resume()
+    keypress(process.stdin)
+    process.stdin.on('keypress', (ch, key) => {
+        kbHandler(ch, key)
+    })
+}
+
+function menuDone(selection) {
+    process.stdout.clearScreenDown()
+    console.log('Selection: ' + selection + ' - ' + items[selection])
+    showCursor()
+    process.exit(0)
+}
+
+let items = []
+
+function main() {
+    let loremIpsum =
+        'Lorem ipsum dolor sit amet ' +
+        'consectetur adipiscing elit sed do eiusmod ' +
+        'tempor incididunt ut labore et dolore magna aliqua ' +
+        '\x1b[1mlong/bright/option\x1b[m ' +
+        'potato'
+    items = loremIpsum.split(' ')
+    let { columns, columnWidth } = computeTableLayout(items)
+    hideCursor()
+    let menuKeyHandler = tableMenu({
+        items,
+        columns,
+        columnWidth,
+        // columns: 3,
+        // columnWidth: 12,
+        done: menuDone
+    }).keyHandler
+    listenKeyboard(menuKeyHandler)
+}
+
+main()
